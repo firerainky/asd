2bcad88a96924fbbfec2a7bbc0d07c0bde6c2b06
Administrator <admin@example.com>
2023-04-28 01:44:56 +0000

    feat: add integer_interface

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
 
+#include "src/math/big_integer.h"
+#include "src/math/native_integer.h"
+#include <iostream>
+#include <iosfwd>
+
+using namespace zhejiangfhe;
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    NativeInteger<u_int32_t> testIntA("123");
+    NativeInteger<u_int32_t> testIntB("1");
+    NativeInteger ans = testIntA.Add(testIntB);
+    std::cout << testIntA.ConvertToString().c_str() << std::endl;
+    std::cout << ans.ConvertToString().c_str() << std::endl;
+
+
+    BigInteger<u_int32_t> testBigIntA("123456789123");
+    std::cout << testBigIntA.ConvertToString().c_str() << std::endl;
+
     return 0;
 }
---
+++ src/math/native_integer.h
@@ -0,0 +1,83 @@
+//
+// Created by 赵启明 on 2023/4/27.
+//
+
+#ifndef ZJ_FHE_LIB_NATIVE_INTEGER_H
+#define ZJ_FHE_LIB_NATIVE_INTEGER_H
+
+#include "integer_interface.h"
+#include "../util/exception.h"
+
+namespace zhejiangfhe {
+    template<typename NativeInt>
+    class NativeInteger : public IntegerInterface<NativeInteger<NativeInt>> {
+
+    public:
+        NativeInteger(const std::string &strValue) {
+            AssignVal(strValue);
+        }
+
+        NativeInteger<NativeInt> Add(const NativeInteger<NativeInt> &b) {
+            return NativeInteger("0");
+        }
+        const NativeInteger<NativeInt> &AddEq(const NativeInteger<NativeInt> &b) {
+            return NativeInteger("0");
+
+        }
+        NativeInteger<NativeInt> Sub(const NativeInteger<NativeInt> &b) const {
+            return NativeInteger("0");
+        }
+        const NativeInteger<NativeInt> &SubEq(const NativeInteger<NativeInt> &b) {
+            return NativeInteger("0");
+        }
+        NativeInteger<NativeInt> Mul(const NativeInteger<NativeInt> &b) const {
+            return NativeInteger("0");
+        }
+        const NativeInteger<NativeInt> &MulEq(const NativeInteger<NativeInt> &b) {
+            return NativeInteger("0");
+        }
+        NativeInteger<NativeInt> DividedBy(const NativeInteger<NativeInt> &b) const {
+            return NativeInteger("0");
+        }
+        const NativeInteger<NativeInt> &DividedByEq(const NativeInteger<NativeInt> &b) {
+            return NativeInteger("0");
+        }
+
+
+        const std::string ConvertToString() {
+            return "NativeInteger test";
+        }
+
+
+    protected:
+        void AssignVal(const std::string &string) {
+            std::string str = string;
+            if (str[0] == '-') {
+                sign = true;
+                str = string.substr(1);
+            }
+            NativeInt test_value = 0;
+            value = 0;
+            for (size_t i = 0; i < str.length(); i++) {
+                int v = str[i] - '0';
+                if (v < 0 || v > 9) {
+                    ZJFHE_THROW(zhejiangfhe::TypeException, "String contains a non-digit");
+                }
+                value *= 10;
+                value += v;
+
+                if (value < test_value) {
+                    ZJFHE_THROW(zhejiangfhe::MathException, str + " is too large to fit in this native integer object");
+                }
+                test_value = value;
+            }
+        }
+
+
+    private:
+        NativeInt value;
+        bool sign;
+    };
+
+}
+#endif//ZJ_FHE_LIB_NATIVE_INTEGER_H
