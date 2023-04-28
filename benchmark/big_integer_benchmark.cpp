a537797bb946afe3004beed6fe00c20666d6d9eb
zhengkanyan <zhengkanyan@zhejianglab.com>
2023-04-28 15:36:49 +0800

    Add benchmark

---
+++ benchmark/big_integer_benchmark.cpp
@@ -0,0 +1,12 @@
+#include <benchmark/benchmark.h>
+
+#include "big_integer.h"
+
+static void BM_BigInt0Creation(benchmark::State &state) {
+    for (auto _: state)
+        zhejiangfhe::BigInteger<u_int32_t> zero;
+}
+// Register the function as a benchmark
+BENCHMARK(BM_BigInt0Creation);
+
+BENCHMARK_MAIN();
