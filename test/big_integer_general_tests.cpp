bb12fdd5a7b9b0392f9409a1519a0caeab7df9aa
zhengkanyan <zhengkanyan@zhejianglab.com>
2023-05-18 09:58:49 +0800

    Extract the limb types list to a common header file for eliminating the duplicate test codes

--- test/big_integer_general_tests.cpp
+++ test/big_integer_general_tests.cpp
@@ -1,17 +1,9 @@
 #include "big_integer.h"
-#include <gtest/gtest.h>
-
-/**
- * 使用 Typed Tests 同时测试不同 limb type 的 big integer
- * reference: https://github.com/google/googletest/blob/main/docs/advanced.md#typed-tests
- */
+#include "big_integer_test.h"
 
 // 创建继承自 Test 的 test fixture
 template<typename T>
 class BigIntegerTest : public testing::Test {};
-
-// 需要测试的 limb_type 类型列表，目前仅需要 uint32 和 uint64
-using LimbTypes = ::testing::Types<uint32_t, uint64_t>;
 
 // 将 test fixture 与类型列表绑定在一起，这样下面的测试就可以自动对类型列表中的每一种都执行一遍了
 TYPED_TEST_SUITE(BigIntegerTest, LimbTypes);
---
+++ test/big_integer_test.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+/**
+ * 使用 Typed Tests 同时测试不同 limb type 的 big integer
+ * reference: https://github.com/google/googletest/blob/main/docs/advanced.md#typed-tests
+ */
+
+// 需要测试的 limb_type 类型列表，目前仅需要 uint32 和 uint64
+using LimbTypes = ::testing::Types<uint32_t, uint64_t>;
