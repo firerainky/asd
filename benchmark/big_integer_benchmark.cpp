b640ae15cd01a8bbe96f5a22dd3e02804bc8a5ed
zhengkanyan <zhengkanyan@zhejianglab.com>
2023-05-31 14:56:38 +0800

    Complete a simple and impractical  benchmark test

--- benchmark/big_integer_benchmark.cpp
+++ benchmark/big_integer_benchmark.cpp
@@ -13,7 +13,10 @@ BENCHMARK(BM_BigInt0Creation);
 static void BM_BigIntModMulDirect(benchmark::State &state) {
     using BInt = zhejiangfhe::BigInteger<uint64_t>;
 
-    BInt a = 3, b = 4, modulus = 5;
+    // BInt a = 3, b = 4, modulus = 5;
+    BInt a = BInt("123456789012345678901234567890");
+    BInt b = BInt("987654321098765432109876543210");
+    BInt modulus = BInt("9999999999999999999912");
 
     for (auto _: state) {
         BInt ans = (a * b) % modulus;
@@ -23,21 +26,15 @@ BENCHMARK(BM_BigIntModMulDirect);
 
 static void BM_BigIntModMulWithModulus(benchmark::State &state) {
     using BInt = zhejiangfhe::BigInteger<uint64_t>;
-    // using Modulus = zhejiangfhe::Modulus<uint64_t>;
-
-    // BInt a = 3, b = 4, m = 5;
-    // Modulus modulus = Modulus(m);
-
-    // for (auto _: state) {
-    //     BInt ans = zhejiangfhe::ModMul(a, b, modulus);
-    // }
-
     using Modulus = zhejiangfhe::Modulus<uint64_t>;
 
-    BInt a = 3, b = 4, modulus = 5;
+    // BInt a = 3, b = 4, modulus = 5;
+    BInt a = BInt("123456789012345678901234567890");
+    BInt b = BInt("987654321098765432109876543210");
+    BInt modulus = BInt("9999999999999999999912");
 
     for (auto _: state) {
-        // BInt ans = zhejiangfhe::ModMul2(a, b, modulus);
+        BInt ans = a.ModMul(b, modulus);
     }
 }
 BENCHMARK(BM_BigIntModMulWithModulus);
