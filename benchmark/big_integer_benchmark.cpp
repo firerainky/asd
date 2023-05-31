dc73c7978a8c01f2742ce6e868893304cb5a4c5c
zhengkanyan <zhengkanyan@zhejianglab.com>
2023-05-31 17:05:32 +0800

    Passing arbitrary arguments to ModMul benchmarks

--- benchmark/big_integer_benchmark.cpp
+++ benchmark/big_integer_benchmark.cpp
@@ -10,33 +10,36 @@ static void BM_BigInt0Creation(benchmark::State &state) {
 // Register the function as a benchmark
 BENCHMARK(BM_BigInt0Creation);
 
-static void BM_BigIntModMulDirect(benchmark::State &state) {
+template<class... Args>
+static void BM_ModMul_Direct(benchmark::State &state, Args &&...args) {
     using BInt = zhejiangfhe::BigInteger<uint64_t>;
 
-    // BInt a = 3, b = 4, modulus = 5;
-    BInt a = BInt("123456789012345678901234567890");
-    BInt b = BInt("987654321098765432109876543210");
-    BInt modulus = BInt("9999999999999999999912");
+    auto args_tuple = std::make_tuple(std::move(args)...);
+    BInt a(std::get<0>(args_tuple));
+    BInt b(std::get<1>(args_tuple));
+    BInt modulus(std::get<2>(args_tuple));
 
     for (auto _: state) {
         BInt ans = (a * b) % modulus;
     }
 }
-BENCHMARK(BM_BigIntModMulDirect);
+BENCHMARK_CAPTURE(BM_ModMul_Direct, big_integer_test, "123456789012345678901234567890", "987654321098765432109876543210", "9999999999999999999912");
+BENCHMARK_CAPTURE(BM_ModMul_Direct, small_integer_test, "3", "4", "5");
 
-static void BM_BigIntModMulWithModulus(benchmark::State &state) {
+template<class... Args>
+static void BM_ModMul_OpenFHEMethod(benchmark::State &state, Args &&...args) {
     using BInt = zhejiangfhe::BigInteger<uint64_t>;
-    using Modulus = zhejiangfhe::Modulus<uint64_t>;
 
-    // BInt a = 3, b = 4, modulus = 5;
-    BInt a = BInt("123456789012345678901234567890");
-    BInt b = BInt("987654321098765432109876543210");
-    BInt modulus = BInt("9999999999999999999912");
+    auto args_tuple = std::make_tuple(std::move(args)...);
+    BInt a(std::get<0>(args_tuple));
+    BInt b(std::get<1>(args_tuple));
+    BInt modulus(std::get<2>(args_tuple));
 
     for (auto _: state) {
         BInt ans = a.ModMul(b, modulus);
     }
 }
-BENCHMARK(BM_BigIntModMulWithModulus);
+BENCHMARK_CAPTURE(BM_ModMul_OpenFHEMethod, big_integer_test, "123456789012345678901234567890", "987654321098765432109876543210", "9999999999999999999912");
+BENCHMARK_CAPTURE(BM_ModMul_OpenFHEMethod, small_integer_test, "3", "4", "5");
 
 BENCHMARK_MAIN();
