cb8bfc7615f5c9c10503faf2d37ddb1bca67ac6c
zhengkanyan <zhengkanyan@zhejianglab.com>
2023-06-01 19:38:20 +0800

    Make ModBarrett64 method inline to eliminate compile error about multiple definition of the function

--- src/math/big_integer_modop.h
+++ src/math/big_integer_modop.h
@@ -26,8 +26,7 @@ namespace zhejiangfhe {
             return ret < modulus.GetValue() ? ret : ret - modulus.GetValue();
         }
 
-
-        uint64_t ModBarrett64(uint64_t &operand, const Modulus<uint64_t> &modulus) {
+        inline uint64_t ModBarrett64(uint64_t &operand, const Modulus<uint64_t> &modulus) {
             uint64_t modulusValue = modulus.GetValue().getValueOfIndex(0);
             uint64_t ration1 = modulus.GetConstRatio().getValueOfIndex(1);
             __int128 estimated = static_cast<__int128>(operand) * static_cast<__int128>(ration1);
